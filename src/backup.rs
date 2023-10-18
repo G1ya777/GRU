2d6df9393adde90a0c1110ade0df9290b6b945da
g1ya777 <g1ya777@protonmail.com>
2023-10-18 17:25:46 +0100

    v0.2.2

--- src/backup.rs
+++ src/backup.rs
@@ -11,7 +11,7 @@ pub fn backup(filenames_list: &Vec<String>, location: &String) {
     let backup_path = {
         let mut path = PathBuf::new();
         path.push(location);
-        path.push("gru_".to_string() + &Utc::now().to_string().replace(" ", "_"));
+        path.push("gru-".to_string() + &Utc::now().to_string().replace(" ", "-"));
         path.set_extension("json");
         path
     };
